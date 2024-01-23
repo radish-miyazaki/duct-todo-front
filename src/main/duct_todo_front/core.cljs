bf14f731e2d66674f5839797cbab1c06e4a97240
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-23 19:03:02 +0900

    setup navigation & routing, re-frame

--- src/main/duct_todo_front/core.cljs
+++ src/main/duct_todo_front/core.cljs
@@ -1,5 +1,43 @@
-(ns duct-todo-front.core)
+(ns duct-todo-front.core
+  (:require
+   [accountant.core :as accountant]
+   [bidi.bidi :as bidi]
+   [duct-todo-front.config :as config]
+   [duct-todo-front.events :as events]
+   [duct-todo-front.routes :refer [routes]]
+   [duct-todo-front.views :as views]
+   [re-frame.core :as re-frame]
+   [reagent.core :as reagent]
+   [reagent.dom.client :as client]))
+
+(defonce root (client/create-root (.getElementById js/document "app")))
+
+(defn- dev-setup []
+  (when config/debug?
+    (enable-console-print!)
+    (println "dev mode")))
+
+(defn- mount-root
+  "The `:dev/after-load` metadata causes this function to be called after
+  shadow-cljs hot-reloads code. This function is called implicitly by its
+  annotation.
+  @ref https://stackoverflow.com/a/72477660"
+  []
+  (re-frame/clear-subscription-cache!)
+  (.render root (reagent/as-element [views/main-panel])))
+
+(defn ^:dev/after-load re-render []
+  (mount-root))
 
 (defn ^:export init []
-  (.log js/console "Hello, World!"))
+  (re-frame.core/dispatch-sync [::events/initialize-db])
+  (accountant/configure-navigation!
+   {:nav-handler (fn [path]
+                   (re-frame/dispatch [::events/set-current-route (bidi/match-route routes path)]))
+    :path-exists? (fn [path]
+                    (boolean (bidi/match-route routes path)))
+    :reload-same-path? true})
+  (accountant/dispatch-current!)
+  (dev-setup)
+  (mount-root))
 
