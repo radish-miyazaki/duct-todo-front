ff774123b96eae99f8db3fdc1f89cbc91ba384a0
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-25 22:35:27 +0900

    apply mui components to page view components

--- src/main/duct_todo_front/views.cljs
+++ src/main/duct_todo_front/views.cljs
@@ -1,27 +1,59 @@
 (ns duct-todo-front.views
   (:require
+   ["@mui/icons-material/Add$default" :as AddIcon]
+   ["@mui/icons-material/Delete$default" :as DeleteIcon]
+   ["@mui/icons-material/Save$default" :as SaveIcon]
+   ["@mui/icons-material/Work$default" :as WorkIcon]
+   ["@mui/material" :refer [AppBar Avatar Breadcrumbs Button Container Dialog
+                            DialogActions DialogTitle Fab IconButton Link List
+                            ListItem ListItemAvatar ListItemSecondaryAction ListItemText TextField Toolbar
+                            Typography]]
    [duct-todo-front.events :as events]
+   [duct-todo-front.routes :as routes]
    [duct-todo-front.subs :as subs]
    [fork.reagent :as fork]
-   [re-frame.core :as re-frame]))
+   [re-frame.core :as re-frame]
+   [reagent.core :as reagent]))
 
 (defmulti view :handler)
 
-(defmethod view ::home [_]
-  [:div "Home"
-   [:ul
-    [:li [:button {:on-click #(re-frame/dispatch [::events/navigate ::list])} "View"]]
-    [:li [:button {:on-click #(re-frame/dispatch [::events/navigate ::create])} "Create"]]]])
-
 (defmethod view ::list [_]
-  [:div "Todo List"
-   [:ul
+  [:div
+   [:p "Todo List"]
+   [:> List
     (map (fn [{:keys [id title]}]
-           [:li {:key id}
-            title
-            [:button {:on-click #(re-frame/dispatch [::events/navigate ::edit {:id id}])} "Edit"]
-            [:button {:on-click #(re-frame/dispatch [::events/delete-todo-by-id id])} "Delete"]])
-         @(re-frame/subscribe [::subs/todos]))]])
+           ^{:key id}
+           [:> ListItem {:button true
+                         :on-click #(re-frame/dispatch [::events/navigate ::edit {:id id}])}
+            [:> ListItemAvatar
+             [:> Avatar
+              [:> WorkIcon]]]
+            [:> ListItemText {:primary title}]
+            [:> ListItemSecondaryAction
+             [:> IconButton {:edge "end"
+                             :aria-label "delete"
+                             :on-click #(re-frame/dispatch [::events/open-delete-dialog id])}
+              [:> DeleteIcon]]]])
+         @(re-frame/subscribe [::subs/todos]))]
+   [:> Dialog {:open @(re-frame/subscribe [::subs/delete-dialog-open?])
+               :on-close #(re-frame/dispatch [::events/close-delete-dialog])
+               :aria-labelledby "alert-dialog-title"}
+    [:> DialogTitle {:id "alert-dialog-title"}
+     "Are you sure you want to delete this?"]
+    [:> DialogActions
+     [:> Button {:on-click #(re-frame/dispatch [::events/close-delete-dialog])}
+      "Cancel"]
+     [:> Button {:on-click (fn []
+                             (re-frame/dispatch [::events/delete-todo-by-id
+                                                 @(re-frame/subscribe [::subs/delete-target])])
+                             (re-frame/dispatch [::events/close-delete-dialog]))
+                 :variant "contained"
+                 :color "error"}
+      "Delete"]]]
+    [:> Fab {:color "primary"
+             :aria-label "add"
+             :on-click #(re-frame/dispatch [::events/navigate ::create])}
+     [:> AddIcon]]])
 
 (defn todo-form [props]
   [fork/form (merge {:prevent-default? true
@@ -35,30 +67,53 @@
               handle-submit]}]
     [:form {:id form-id
             :on-submit handle-submit}
-     [:label "Title"]
-     [:input {:name "title"
-              :value (values "title")
-              :on-change handle-change
-              :on-blur handle-blur}]
-     [:button {:type "submit"
-               :disabled submitting?}
-      "Submit"]]) ])
+     [:> TextField {:label "Title"
+                    :required true
+                    :name "title"
+                    :default-value (values "title")
+                    :on-change handle-change
+                    :on-blur handle-blur}]
+     [:> Button {:type "submit"
+                 :disabled submitting?
+                 :variant "contained"
+                 :start-icon (reagent/as-element [:> SaveIcon])}
+      "Save"]]) ])
 
 (defmethod view ::create [_]
-  [:div "Create New Todo"
+  [:div
+   [:p "Create New Todo"]
    [todo-form {:on-submit #(re-frame/dispatch [::events/create-todo %])}]])
 
 (defmethod view ::edit [{:keys [route-params]}]
-  [:div (str "Edit Todo " (:id route-params))
+  [:div
+   [:p (str "Edit Todo " (:id route-params))]
    (when-let [{:keys [id title]} @(re-frame/subscribe [::subs/selected-todo])]
      [todo-form {:initial-values {"title" title}
                  :on-submit #(re-frame/dispatch [::events/update-todo-by-id id %])}])])
 
 (defmethod view :default [_]
-  [:div "404 Not Found"])
+  [:div
+   [:p "404 Not Found"]])
 
 (defn main-panel []
-  [:div
-   [:div {:on-click #(re-frame/dispatch [::events/navigate ::home])} "Todo App"]
-   [view @(re-frame/subscribe [::subs/current-route])]])
+  (let [{:keys [handler] :as current-route} @(re-frame/subscribe [::subs/current-route])]
+    [:div
+     [:> AppBar {:position "static"}
+      [:> Toolbar
+       [:> Breadcrumbs {:aria-label "breadcrum"}
+        [:> Link {:color "white"
+                  :variant "h6"
+                  :underline "none"
+                  :href (routes/path-for ::list)}
+         "TODOS"]
+        (case handler
+          ::create [:> Typography {:color "white"
+                                   :variant "h6"}
+                    "CREATE"]
+          ::edit [:> Typography {:color "white"
+                                 :variant "h6"}
+                  "EDIT"]
+          nil)]]]
+     [:> Container
+      [view current-route]]]))
 
